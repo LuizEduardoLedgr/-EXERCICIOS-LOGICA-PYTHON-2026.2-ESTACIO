6200ff14fee09c1867760d64c7393249e2469be5
LuizEduardoLedgr <cunha.led@gmail.com>
2026-09-07 15:38:12 -0300

    Create Conversão entre escalas termométricas.py
    Conversão entre escalas termométricas

---
+++ Unidade 1/Conversão entre escalas termométricas.py
@@ -0,0 +1,6 @@
+A = float(input("digite a temperatura em Celsius: "))
+B = (A * 9/5) + 32
+C = (A + 273.15)
+print("------- Os resultados são -------")
+print("A temperatura em Fahrenheit é:", B)
+print("A temperatura em Kelvin é:", C)\n
