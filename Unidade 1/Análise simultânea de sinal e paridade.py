0b6e646ce3026acb12f1316d82f543af224d8609
LuizEduardoLedgr <cunha.led@gmail.com>
2026-09-08 21:14:33 -0300

    Create Análise simultânea de sinal e paridade.py
    Análise simultânea de sinal e paridade

---
+++ Unidade 1/Análise simultânea de sinal e paridade.py
@@ -0,0 +1,11 @@
+A = int(input("digite um numero: "))
+if A > 0:
+    print("o numero é positivo:", A)
+elif A == 0:
+    print("o numero é nulo", A)
+elif A < 0:
+    print("o numero é negativo")
+if A % 2 == 0:
+    print("o numero é par")
+else:
+    print("o numero é impar")
